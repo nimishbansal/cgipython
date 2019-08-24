715be8150f9af9a7ab06c81b6666dc8fcdd24071
nimish <nimish4july1998@gmail.com>
2019-08-24 15:09:42 +0530

    added game detail api view

--- main_app/api/urls.py
+++ main_app/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include, re_path
 from main_app.api import views
 
 urlpatterns = [
-    re_path(r'games/', views.GamesListAPIView.as_view()),
+    re_path(r'games/$', views.GamesListAPIView.as_view()),
+    re_path(r'games/(?P<pk>[0-9]+)/', views.GameDetailAPIView.as_view()),
 ]
--- main_app/api/views.py
+++ main_app/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
@@ -20,3 +20,10 @@ class GamesListAPIView(ListAPIView):
     def get_queryset(self):
         queryset = Game.objects.all()
         return queryset
+
+
+class GameDetailAPIView(RetrieveAPIView):
+    authentication_classes = [BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated, ]
+    serializer_class = GameSerializer
+    queryset = Game.objects.all()
