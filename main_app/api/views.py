a13336e4e6179e6ea30d4dc47b7482aad7448780
nimish <nimish4july1998@gmail.com>
2019-08-24 17:31:18 +0530

    added filters

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
-from main_app.api.serializers import GameSerializer
-from main_app.models import Game
+from main_app.api.serializers import GameSerializer, GamePlatformSerializer, GameGenreSerializer
+from main_app.models import Game, GamePlatform, GameGenre
 
 
 class StandardPagination(PageNumberPagination):
@@ -16,6 +16,16 @@ class GamesListAPIView(ListAPIView):
     permission_classes = [IsAuthenticated, ]
     serializer_class = GameSerializer
     pagination_class = StandardPagination
+
+    def get(self, request, *args, **kwargs):
+        res = super(GamesListAPIView, self).get(request, *args, **kwargs)
+        platforms = GamePlatformSerializer(GamePlatform.objects.all(), many=True)
+        genres = GameGenreSerializer(GameGenre.objects.all(), many=True)
+        editors_choice = ['Y', 'N']
+        res.data.update({"platforms": platforms.data})
+        res.data.update({"genres": genres.data})
+        res.data.update({"editors_choice": editors_choice})
+        return res
 
     def get_queryset(self):
         queryset = Game.objects.all()
--- main_app/views.py
+++ main_app/views.py
@@ -5,14 +5,21 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 # from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from main_app.models import Game
+from main_app.models import Game, GamePlatform, GameGenre
 
 
 @api_view(('GET', 'POST'))
 @authentication_classes((BasicAuthentication, TokenAuthentication))
 @permission_classes((IsAuthenticated,))
 def home(request):
-    a = Game.objects.first()
-    return render(request, 'main_app/home.html')
+    platforms = GamePlatform.objects.all()
+    genres = GameGenre.objects.all()
+    editors_choices = ['Y', 'N']
+    context = {
+        'platforms': platforms,
+        'genres': genres,
+        'editors_choices': editors_choices
+    }
+    return render(request, 'main_app/home.html', context=context)
     # return Response('ok')
 
